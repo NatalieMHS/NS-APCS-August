f5addaf3c2e57a6527f934af5cbcfb40487f4341
Natalie Suboc <19subocn38@millshighsch.org>
2017-08-24 09:30:01 -0700

    When you try your best but you don't succeed

---
+++ Project_2B/src/Project_2B.java
@@ -0,0 +1,24 @@
+/* Natalie Suboc
+*  August 24, 2017
+*  Project 2B -- using print and println
+*/
+
+// import java.util.ArrayList;
+
+public class Project_2B {
+
+	public static void main(String[] args) {
+		System.out.println("Hullo wurld");
+		System.out.print("IDJIhw uwehfuiwehfuiwfhuiwfh eee");
+
+	}
+
+	/*public static void notSureWhatImDoing() {
+		System.out.println("Hello!");
+		ArrayList numList = new ArrayList();
+		for (int i = 0; i < 5; i++) {
+			numList.add(i);
+		}
+		System.out.println(numList);
+	}*/
+}
