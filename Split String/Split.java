c24a51695305e77c5a6e005c43c9b0514506f7b8
NatalieSuboc <nsuboc@gmail.com>
2017-10-29 16:45:38 -0700

    Split String Pts. 1 and 2

--- Split String/Split.java
+++ Split String/Split.java
@@ -1,10 +1,10 @@
+
 import java.util.*;
 
 public class Split {
 	
 	public static void main(String[] args) {
-		String sandwich = "applesbreadpineapplesbreadlettustomatobaconbreadmayohambreadcheese";
-		System.out.println(sandwichMiddle(sandwich));
+		String sandwich = "applesbreadpineappleslettustomatobaconmayohambreadcheese";
 
 		//Your task Part 1:
 		/*Write a method that take in a string like "applespineapplesbreadlettustomatobaconmayohambreadcheese" describing a sandwich
@@ -12,20 +12,54 @@ public class Split {
 		* What if it's a fancy sandwich with multiple pieces of bread?
 		*/
 
+		System.out.println(sandwichMiddle(sandwich));
 
 		//Your task Part 2:
 		/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich
 		* use String.split to split up the sandwich at the spaces, " ", and return what's in the middle of the sandwich and ignores what's on the outside
 		* Again, what if it's a fancy sandwich with multiple pieces of bread?
 		*/
+		
+		String sandwich2 = "apples pineapples bread lettus tomato bacon mayo ham bread cheese";
+		System.out.println(sandwichMiddle2(sandwich2));
 	}
 	
 	public static String sandwichMiddle(String sandwich) {
 		String[] toppings = new String[sandwich.split("bread").length];
-		//String middle = "";
+		String middle = ""; // everything in the middle of the sandwich excluding bread
 		for (int i = 0; i < toppings.length; i++) {
 			toppings[i] = sandwich.split("bread")[i];
 		}
-		return toppings[1];
+		for (int i = 1; i < toppings.length-1; i++) {
+			middle += toppings[i] + " ";
+		}
+		return middle;
+	}
+	
+	public static String sandwichMiddle2(String sandwich) {
+		String[] toppings = new String[sandwich.split(" ").length];
+		int numBread = 0;
+		for (int i = 0; i < toppings.length; i++) {
+				toppings[i] = sandwich.split(" ")[i];
+				if (toppings[i].equals("bread")) {
+					numBread++;
+				}
+		}
+		
+		int[] breadIndices = new int[numBread];
+		int counter = 0;
+		for (int i = 0; i < toppings.length; i++) {
+			if (toppings[i].equals("bread")) {
+				breadIndices[counter] = i;
+				counter++;
+			}
+		}
+		
+		String middle = "";
+		for (int i = breadIndices[0] + 1; i < breadIndices[breadIndices.length - 1]; i++) {
+			middle += toppings[i] + " ";
+		}
+		
+		return middle;
 	}
 }
