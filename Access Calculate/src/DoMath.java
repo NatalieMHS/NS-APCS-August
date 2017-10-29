178172d95bee3da4e046d11b31c1e517f60993fa
NatalieSuboc <nsuboc@gmail.com>
2017-10-29 15:54:54 -0700

    Idk

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -18,7 +18,7 @@ public class DoMath {
 		System.out.println(Calculate.toDegrees(2 * 3.14159));
 		System.out.println(Calculate.toRadians(-180));
 		System.out.println(Calculate.discriminant(1, 4, 4));
-		System.out.println(Calculate.toMixedNum(-7, 2));
+		System.out.println(Calculate.gcf(2, 1));
 		System.out.println(Calculate.foil(2,  3,  6,  -7,  "n"));
 	}
 }
