c53705001746ebb81487185b764978d17bcc077b
Natalie Suboc <19subocn38@millshighsch.org>
2017-10-30 09:21:20 -0700

    Array Mini-Lab setup

---
+++ Array Minilab/src/LotsOfCopies.java
@@ -0,0 +1,15 @@
+
+public class LotsOfCopies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 7;
+		String strMain = "APCS";
+		int[] arrMain = {1, 2, 3, 4, 5};
+	}
+	
+	public static void changeMe(int x, String str, int[] arr) {
+		System.out.println();
+	}
+
+}
