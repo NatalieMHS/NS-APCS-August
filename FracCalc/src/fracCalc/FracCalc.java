13ca3df39754f5e6eb33643b61d334070468e13f
Natalie Suboc <19subocn38@millshighsch.org>
2017-11-17 09:52:29 -0800

    Frac Calc Pt. 3 in progress

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,11 +1,17 @@
+/* Natalie Suboc
+ * November 13, 2017
+ * Fraction calculator that takes two fractions and
+ * 
+ */
+
 package fracCalc;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class FracCalc {
 
     public static void main(String[] args) {
-        // TODO: Read the input from the user and call produceAnswer with an equation
     	Scanner input = new Scanner(System.in);
     	String quit = "";
     	do {
@@ -29,16 +35,34 @@ public class FracCalc {
         // TODO: Implement this function to produce the solution to the input
         String[] splitExpr = input.split(" ");
         String firstFrac = splitExpr[0];
-        String operation = splitExpr[1];
+        String operand = splitExpr[1];
         String secFrac = splitExpr[2];
         
-        String[] splitFrac1 = parseFrac(firstFrac);
+        String[] splitFrac1 = parseFrac(firstFrac); 
         String[] splitFrac2 = parseFrac(secFrac);
+        int[] fracNums1 = new int[3]; 
+        int[] fracNums2 = new int[3];
         
-        return "whole:" + splitFrac2[0] + " numerator:" + splitFrac2[1] + " denominator:" + splitFrac2[2];
+        for (int i = 0; i < splitFrac1.length; i++) {
+        	fracNums1[i] = Integer.parseInt(splitFrac1[i]);
+        	fracNums2[i] = Integer.parseInt(splitFrac2[i]);
+        }
+        
+        int[] impFrac1 = toImproperFrac(fracNums1[0], fracNums1[1], fracNums1[2]);
+        int[] impFrac2 = toImproperFrac(fracNums2[0], fracNums2[1], fracNums2[2]);
+        System.out.println(Arrays.toString(impFrac1) + " " + Arrays.toString(impFrac2));
+        if (operand.equals("/") || operand.equals("*")) {
+        	// return multiplyOrDivide(fracNums1, operand, fracNums2);
+        } else if (operand.equals("+") || operand.equals("-")) {
+        	System.out.println(addOrSubtract(impFrac1, operand, impFrac2));
+        } else {
+        	// return "Not a valid operator";
+        }
+        
+        return "whole:"+ splitFrac2[0] + " numerator:" + splitFrac2[1] + " denominator:" + splitFrac2[2];
     }
 
-    // TODO: Fill in the space below with any helper methods that you think you will need
+    // takes a fraction and returns the whole number, numerator, and denominator of it
     public static String[] parseFrac(String fraction) {
     	String whole = "0";
     	String denom = "1";
@@ -60,4 +84,34 @@ public class FracCalc {
     	return parsedFrac;
     	
     }
+    
+    // changes a mixed number to an improper fraction
+	public static int[] toImproperFrac(int wholeNum, int numerator, int denominator) {
+		int improper = (wholeNum * denominator);
+		if (numerator < 0 || denominator < 0) {
+			throw new IllegalArgumentException("The numerator or denominator of a mixed number cannot be negative.");
+		}
+		if (wholeNum >= 0) {
+			improper += numerator;
+		} else {
+			improper -= numerator;
+		}
+		int[] impFrac = {improper, denominator};
+		return impFrac;
+	}
+	public static String addOrSubtract(int[] frac1, String operand, int[] frac2) {
+		int comDenom = frac1[1] * frac2[1];
+		System.out.println(comDenom);
+		frac1[0] *= frac2[1];
+		frac2[0] *= frac1[1];
+		int numer = 0;
+		if (operand.equals("-")) {
+			numer = frac1[0] - frac2[0];
+		}
+		if (operand.equals("+")) {
+			numer = frac1[0] + frac2[0];
+		}
+		System.out.println(numer);
+		return numer + "/" + comDenom;
+	}
 }
