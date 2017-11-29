b811f7e320a99983eef6d74b679542abe548156f
Natalie Suboc <19subocn38@millshighsch.org>
2017-11-29 10:20:08 -0800

    DrumKit

--- DrumKitTestDrive/src/DrumKitTestDrive.java
+++ DrumKitTestDrive/src/DrumKitTestDrive.java
@@ -2,7 +2,6 @@
 public class DrumKitTestDrive {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		DrumKit d = new DrumKit();
 		d.playSnare();
 		d.snare = false;
